72f9ca628c7972dea56202772ce69bba630f909e
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-10-21 20:13:25 +0600

    Updated title and some meta tags

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,5 @@
 export const EXAMPLE_PATH = "blog-starter-typescript";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL =
-  "https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
+export const HOME_OG_IMAGE_URL = "https://black-screen.vercel.app/og-image.jpg";
 export const DEFAULT_URL = "https://black-screen.vercel.app/";
 export const DEFAULT_GITHUB_URL = "https://github.com/Prottoy2938/black-screen";
