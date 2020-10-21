f2ad2dee8f12301f5eb9b6031dfc3575f5488ecb
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-10-21 20:25:26 +0600

    Updated meta image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const EXAMPLE_PATH = "blog-starter-typescript";
 export const CMS_NAME = "Markdown";
-export const HOME_OG_IMAGE_URL = "https://black-screen.vercel.app/og-image.jpg";
+export const HOME_OG_IMAGE_URL = "https://blog.edrini.xyz/og-image.jpg";
 export const DEFAULT_URL = "https://black-screen.vercel.app/";
 export const DEFAULT_GITHUB_URL = "https://github.com/Prottoy2938/black-screen";
