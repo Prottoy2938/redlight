aeebb972965ba60f569d1de3bc60448e537e1faf
Prottay Rudra <prottoyrudra7777@gmail.com>
2021-02-12 19:56:13 +0600

    Updating existential crisis app

---
+++ _posts/existential-crisis-and-some-music-to-extend-it.md
@@ -0,0 +1,54 @@
+---
+title: " Existential Crisis and Some Musics to Extend it"
+excerpt: "Mosquitos are having a concert"
+metaDescription: "Mosquitos are having a concert"
+coverImage: "/assets/blog/movies-similar-to-ratatouille/ratatouille-cover.jpg"
+date: "2021-12-02T05:35:07.322Z"
+author:
+  name: Svesp
+  picture: "/assets/blog/authors/svesp.jpg"
+ogImage:
+  url: "https://redlight.vercel.app/assets/blog/existential-crisis-and-some-music-to-extend-it/theme.jpg"
+url: "https://redlight.vercel.app/posts/existential-crisis-and-some-music-to-extend-it"
+---
+
+blog\existential-crisis-and-some-music-to-extend-it
+
+> Mosquitos are having a concert
+
+# Existential Crisis and Some Musics to Extend it
+
+Thinking about your existence? Why are you in this universe? What's your purpose?
+
+This is a feeling that happens to some of us often.
+
+Why does this happen? What goes on through your mind during this existential crisis?
+
+- I don't know
+
+Do We enter a new dimension during this typeof thoughts? Don't know. Do we become philosopher during this period? Don't know that one too.
+
+People tend to be alone when they reach this state.
+
+## Bringing Up Existential Crisis
+
+Why and Where does this feeling comes from?
+I don't have a solid answer. But heres some ways I've seen which brings up this kinda thoughts.
+
+**[Kurzgesagt – In a Nutshell](https://www.youtube.com/user/Kurzgesagt)** , could watch some contents from this channel.
+
+Maybe try out this [video](https://www.youtube.com/watch?v=v3y8AIEX_dU).
+
+## Extending Existential Crisis
+
+Heres some music that might help some of you enjoying the existential crisis.
+
+- [Lights of the Seven](https://www.youtube.com/watch?v=pS-gbqbVd8c)
+- [Interstellar Soundtrack by Hans Zimmer](https://www.youtube.com/watch?v=UDVtMYqUAyw)
+- [Water - Ludwig Göransson](https://youtu.be/aVMkvCTT_yg?t=1137) \*timestamp included
+
+## Getting out of It
+
+I don't know a clear way. I think you should hang out with friends, go the park, travel, help people, or get a pet.
+
+If you want to know what goes on others people's mind during this time, check out this [subreddit](https://www.reddit.com/r/philosophy/).
--- components/footer.tsx
+++ components/footer.tsx
@@ -11,60 +11,60 @@ type Props = {
 
 const Footer = ({ post }: Props) => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          {post ? (
-            <>
-              <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                {post.title}
-              </h3>
-              <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-                <a
-                  href={post.url}
-                  className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-                >
-                  Visit
-                </a>
-                {post.github && (
-                  <a
-                    href={post.github}
-                    className="mx-3 font-bold hover:underline"
-                  >
-                    View on GitHub
-                  </a>
-                )}
-              </div>
-            </>
-          ) : (
-            <>
-              {/* Default homepage footer */}
-              <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-                {DEFAULT_FOOTER_APP}
-              </h3>
-
-              <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-                <a
-                  href={DEFAULT_URL}
-                  className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-                >
-                  Visit
-                </a>
-                {DEFAULT_GITHUB_URL && (
-                  <a
-                    href={DEFAULT_GITHUB_URL}
-                    className="mx-3 font-bold hover:underline"
-                  >
-                    View on GitHub
-                  </a>
-                )}
-              </div>
-            </>
-          )}
-        </div>
-      </Container>
-    </footer>
+    <footer className="bg-accent-1 border-t border-accent-2">{null}</footer>
   );
 };
 
 export default Footer;
+
+// {/* <Container>
+// <div className="py-28 flex flex-col lg:flex-row items-center">
+//   {post ? (
+//     <>
+//       <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+//         {post.title}
+//       </h3>
+//       <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+//         <a
+//           href={post.url}
+//           className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+//         >
+//           Visit
+//         </a>
+//         {post.github && (
+//           <a
+//             href={post.github}
+//             className="mx-3 font-bold hover:underline"
+//           >
+//             View on GitHub
+//           </a>
+//         )}
+//       </div>
+//     </>
+//   ) : (
+//     <>
+//       {/* Default homepage footer */}
+//       <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+//         {DEFAULT_FOOTER_APP}
+//       </h3>
+
+//       <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+//         <a
+//           href={DEFAULT_URL}
+//           className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+//         >
+//           Visit
+//         </a>
+//         {DEFAULT_GITHUB_URL && (
+//           <a
+//             href={DEFAULT_GITHUB_URL}
+//             className="mx-3 font-bold hover:underline"
+//           >
+//             View on GitHub
+//           </a>
+//         )}
+//       </div>
+//     </>
+//   )}
+// </div>
+// </Container> */}
